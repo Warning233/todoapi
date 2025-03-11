109d9215fa1e189f06018b75b4760450b1bd0ed7
Ruslan Moskaev <fgcgvdj@gmail.com>
2025-03-11 18:36:57 +0300

    connect API with web part

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -6,22 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-// Add OpenAPI
-builder.Services.AddOpenApi();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazor", policy =>
+    {
+        policy.WithOrigins("http://localhost:5204") // Blazor. Paste yours.
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// OpenAPI in development mode
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-app.MapControllers(); 
+app.UseHttpsRedirection(); 
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors("AllowBlazor");
 app.UseAuthorization();
+app.MapControllers();
+
 app.Run();
